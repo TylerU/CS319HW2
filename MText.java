72250445dcfc77c7a4808b03a52e0a420efb4670
Tyler <dinoentrails@gmail.com>
2013-09-29 00:26:24 -0500

    Added classes for menu items

--- MText.java
+++ MText.java
@@ -1,9 +1,96 @@
 
 import java.awt.*;
 import java.awt.event.*;
+
 import javax.swing.*;
 
 
+class MTextFileMenu extends JMenu implements ActionListener{
+	public MTextFileMenu(){
+		super("New");
+		JMenuItem newMenuItem = new JMenuItem("New");
+		newMenuItem.addActionListener(this);
+		add(newMenuItem);
+		
+		JMenuItem openMenuItem = new JMenuItem("Open");
+		openMenuItem.addActionListener(this);
+		add(openMenuItem);
+		
+		JMenuItem saveMenuItem = new JMenuItem("Save");
+		saveMenuItem.addActionListener(this);
+		add(saveMenuItem);
+		
+		JMenuItem exitMenuItem = new JMenuItem("Exit");
+		exitMenuItem.addActionListener(this);
+		add(exitMenuItem);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent evt) {
+		String curAction = evt.getActionCommand();
+		if(curAction.equals("New")){
+			System.out.println("New");
+		}
+		else if(curAction.equals("Open")){
+			System.out.println("Open");
+		}
+		else if(curAction.equals("Save")){
+			System.out.println("Save");
+		}
+		else if(curAction.equals("Exit")){
+			System.out.println("Exit");
+		}
+		else{
+			System.out.println("Unrecognized action event in MTextFileMenu");
+		}
+	}
+}
+
+
+class MTextEditMenu extends JMenu implements ActionListener{
+	public MTextEditMenu(){
+		super("Edit");
+		JMenuItem findMenuItem = new JMenuItem("Find");
+		findMenuItem.addActionListener(this);
+		add(findMenuItem);
+		
+		JMenuItem replaceMenuItem = new JMenuItem("Replace");
+		replaceMenuItem.addActionListener(this);
+		add(replaceMenuItem);
+		
+		JMenuItem replaceAllMenuItem = new JMenuItem("Replace All");
+		replaceAllMenuItem.addActionListener(this);
+		add(replaceAllMenuItem);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent evt) {
+		String curAction = evt.getActionCommand();
+		if(curAction.equals("Find")){
+			System.out.println("FIND");
+		}
+		else if(curAction.equals("Replace")){
+			System.out.println("Replace");
+		}
+		else if(curAction.equals("Replace All")){
+			System.out.println("Replace All");
+		}
+		else{
+			System.out.println("Unrecognized action event in MTextEditMenu");
+		}
+	}
+}
+
+
+class MTextMenu extends JMenuBar
+{
+	public MTextMenu(JFrame frame){
+		JMenu fileMenu = new MTextFileMenu();
+		add(fileMenu);
+		JMenu editMenu = new MTextEditMenu();
+		add(editMenu);
+	}
+}
 
 public class MText {
 	private static final int AREA_COLUMNS = 33;
@@ -16,29 +103,8 @@ public class MText {
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myFrame.getContentPane().setLayout(new FlowLayout());
 		
-		
-		JMenuBar myMenuBar = new JMenuBar();
-		myFrame.setJMenuBar(myMenuBar);
-		JMenu fileMenu = new JMenu("File");
-		myMenuBar.add(fileMenu);
-		JMenu editMenu = new JMenu("Edit");
-		myMenuBar.add(editMenu);
-		
-		JMenuItem newMenuItem = new JMenuItem("New");
-		fileMenu.add(newMenuItem);
-		JMenuItem openMenuItem = new JMenuItem("Open");
-		fileMenu.add(openMenuItem);
-		JMenuItem saveMenuItem = new JMenuItem("Save");
-		fileMenu.add(saveMenuItem);
-		JMenuItem exitMenuItem = new JMenuItem("Exit");
-		fileMenu.add(exitMenuItem);
-
-		JMenuItem findMenuItem = new JMenuItem("Find");
-		editMenu.add(findMenuItem);
-		JMenuItem replaceMenuItem = new JMenuItem("Replace");
-		editMenu.add(replaceMenuItem);
-		JMenuItem replaceAllMenuItem = new JMenuItem("Replace All");
-		editMenu.add(replaceAllMenuItem);
+		MTextMenu menu = new MTextMenu(myFrame);
+		myFrame.setJMenuBar(menu);
 		
 		
 		JTextArea rightTextArea = new JTextArea("right", AREA_ROWS, AREA_COLUMNS);
