9330ce8a8e095136b7fe4ed78c52790a8f3c30cf
Tyler <dinoentrails@gmail.com>
2013-09-29 02:56:49 -0500

    Find/Replace Functionality - ugly implementation

--- MText.java
+++ MText.java
@@ -3,32 +3,201 @@ import java.awt.*;
 import java.awt.event.*;
 
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicTextUI;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultHighlighter;
+import javax.swing.text.Highlighter;
 
 
-class MTextFileMenu extends JMenu implements ActionListener{
-	public MTextFileMenu(){
+class MTextFileMenu extends JMenu{
+	public MTextFileMenu(MTextFrame frame){
 		super("New");
 		JMenuItem newMenuItem = new JMenuItem("New");
-		newMenuItem.addActionListener(this);
+		newMenuItem.addActionListener(frame);
 		add(newMenuItem);
 		
 		JMenuItem openMenuItem = new JMenuItem("Open");
-		openMenuItem.addActionListener(this);
+		openMenuItem.addActionListener(frame);
 		add(openMenuItem);
 		
 		JMenuItem saveMenuItem = new JMenuItem("Save");
-		saveMenuItem.addActionListener(this);
+		saveMenuItem.addActionListener(frame);
 		add(saveMenuItem);
 		
 		JMenuItem exitMenuItem = new JMenuItem("Exit");
-		exitMenuItem.addActionListener(this);
+		exitMenuItem.addActionListener(frame);
 		add(exitMenuItem);
 	}
+}
 
+
+class MTextEditMenu extends JMenu {
+	public MTextEditMenu(MTextFrame frame){
+		super("Edit");
+		JMenuItem findMenuItem = new JMenuItem("Find");
+		findMenuItem.addActionListener(frame);
+		add(findMenuItem);
+		
+		JMenuItem replaceMenuItem = new JMenuItem("Replace");
+		replaceMenuItem.addActionListener(frame);
+		add(replaceMenuItem);
+		
+		JMenuItem replaceAllMenuItem = new JMenuItem("Replace All");
+		replaceAllMenuItem.addActionListener(frame);
+		add(replaceAllMenuItem);
+	}
+}
+
+
+class MTextMenu extends JMenuBar
+{
+	public MTextMenu(MTextFrame frame){
+		JMenu fileMenu = new MTextFileMenu(frame);
+		add(fileMenu);
+		JMenu editMenu = new MTextEditMenu(frame);
+		add(editMenu);
+	}
+}
+
+
+public class MText{
+	public static void main(String[] args){
+		MTextFrame frame = new MTextFrame();
+	}
+}
+
+class MTextFrame extends JFrame implements ActionListener, FocusListener {
+	private static final int AREA_COLUMNS = 33;
+	private static final int AREA_ROWS = 30;
+	private JTextArea leftTextArea;
+	private JTextArea rightTextArea;
+	private Component lastFocused;
+	public MTextFrame(){
+		super("MText");
+		setSize(800,570);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(new FlowLayout());
+		
+		MTextMenu menu = new MTextMenu(this);
+		setJMenuBar(menu);
+		
+		
+		rightTextArea = new JTextArea("right", AREA_ROWS, AREA_COLUMNS);
+		rightTextArea.setLineWrap(true);
+		rightTextArea.addFocusListener(this);
+		JScrollPane rightScrollPane = new JScrollPane(rightTextArea);
+		
+		leftTextArea = new JTextArea("left", AREA_ROWS, AREA_COLUMNS);
+		leftTextArea.setLineWrap(true);
+		leftTextArea.addFocusListener(this);
+		JScrollPane leftScrollPane = new JScrollPane(leftTextArea);
+		
+		JPanel leftPanel = new JPanel();
+		leftPanel.add(leftScrollPane);
+		getContentPane().add(leftPanel);
+		
+		JPanel rightPanel = new JPanel();
+		rightPanel.add(rightScrollPane);
+		getContentPane().add(rightPanel);
+		
+		setVisible(true);
+	}
+	
+	private JTextArea getCurTextArea(){
+		return (JTextArea) lastFocused;
+	}
+	
+	private String getStringToReplace(){
+		JTextArea cur = getCurTextArea();
+		String selectedText = cur.getSelectedText();
+		if(selectedText != null && selectedText.length() > 0){
+			return selectedText;
+		}
+		else{
+			String s = (String)JOptionPane.showInputDialog(
+                    this,
+                    "Enter text which will be replaced",
+                    "Customized Dialog",
+                    JOptionPane.PLAIN_MESSAGE);
+			if(s != null && s.length() > 0){
+				return s;
+			}
+			else{
+				return "";
+			}
+		}
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent evt) {
 		String curAction = evt.getActionCommand();
-		if(curAction.equals("New")){
+		if(curAction.equals("Find")){	  
+			  String s = (String)JOptionPane.showInputDialog(
+	                    this,
+	                    "Enter text to search for",
+	                    "Customized Dialog",
+	                    JOptionPane.PLAIN_MESSAGE);
+		
+			if ((s != null) && (s.length() > 0)) {
+				JTextArea cur = getCurTextArea();
+			
+				String curStr = cur.getText();
+				int where = curStr.indexOf(s, cur.getCaretPosition());
+				if(where >= 0){			
+					cur.setCaretPosition(where);
+					cur.select(where, where + s.length());
+				}
+			}
+		}
+		else if(curAction.equals("Replace")){
+			String from = getStringToReplace();
+			JTextArea cur = getCurTextArea();
+			String text = cur.getText();
+			int indexof = text.indexOf(from, cur.getCaretPosition());
+			if(indexof >= 0){
+				 String s = (String)JOptionPane.showInputDialog(
+		                    this,
+		                    "Enter replacement text",
+		                    "Customized Dialog",
+		                    JOptionPane.PLAIN_MESSAGE);
+				 if(s != null && s.length() > 0){
+					 text = text.substring(0, cur.getCaretPosition()) + text.substring(cur.getCaretPosition(), text.length()).replaceFirst(from, s);
+					 cur.setText(text);
+					 cur.setCaretPosition(indexof);
+				 }
+			}
+			else{
+				JOptionPane.showMessageDialog(this,
+					    "Unable to locate that text",
+					    "Inane error",
+					    JOptionPane.ERROR_MESSAGE);
+			}
+		}
+		else if(curAction.equals("Replace All")){
+			String from = getStringToReplace();
+			JTextArea cur = getCurTextArea();
+			String text = cur.getText();
+			int indexof = text.indexOf(from, cur.getCaretPosition());
+			if(indexof >= 0){
+				 String s = (String)JOptionPane.showInputDialog(
+		                    this,
+		                    "Enter replacement text",
+		                    "Customized Dialog",
+		                    JOptionPane.PLAIN_MESSAGE);
+				 if(s != null && s.length() > 0){
+					 text = text.substring(0, cur.getCaretPosition()) + text.substring(cur.getCaretPosition(), text.length()).replace(from, s);
+					 cur.setText(text);
+					 cur.setCaretPosition(indexof);
+				 }
+			}
+			else{
+				JOptionPane.showMessageDialog(this,
+					    "Unable to locate that text",
+					    "Inane error",
+					    JOptionPane.ERROR_MESSAGE);
+			}		}		
+		else if(curAction.equals("New")){
 			System.out.println("New");
 		}
 		else if(curAction.equals("Open")){
@@ -41,88 +210,18 @@ class MTextFileMenu extends JMenu implements ActionListener{
 			System.out.println("Exit");
 		}
 		else{
-			System.out.println("Unrecognized action event in MTextFileMenu");
-		}
-	}
-}
-
-
-class MTextEditMenu extends JMenu implements ActionListener{
-	public MTextEditMenu(){
-		super("Edit");
-		JMenuItem findMenuItem = new JMenuItem("Find");
-		findMenuItem.addActionListener(this);
-		add(findMenuItem);
-		
-		JMenuItem replaceMenuItem = new JMenuItem("Replace");
-		replaceMenuItem.addActionListener(this);
-		add(replaceMenuItem);
-		
-		JMenuItem replaceAllMenuItem = new JMenuItem("Replace All");
-		replaceAllMenuItem.addActionListener(this);
-		add(replaceAllMenuItem);
+			System.out.println("Unrecognized action event: " + curAction);
+		}		
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent evt) {
-		String curAction = evt.getActionCommand();
-		if(curAction.equals("Find")){
-			System.out.println("FIND");
-		}
-		else if(curAction.equals("Replace")){
-			System.out.println("Replace");
-		}
-		else if(curAction.equals("Replace All")){
-			System.out.println("Replace All");
-		}
-		else{
-			System.out.println("Unrecognized action event in MTextEditMenu");
-		}
+	public void focusGained(FocusEvent evt) {
+		lastFocused = evt.getComponent();
 	}
-}
 
-
-class MTextMenu extends JMenuBar
-{
-	public MTextMenu(JFrame frame){
-		JMenu fileMenu = new MTextFileMenu();
-		add(fileMenu);
-		JMenu editMenu = new MTextEditMenu();
-		add(editMenu);
-	}
-}
-
-public class MText {
-	private static final int AREA_COLUMNS = 33;
-	private static final int AREA_ROWS = 30;
-	
-	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("MText");
-		myFrame.setSize(800,570);
+	@Override
+	public void focusLost(FocusEvent arg0) {
+		// TODO Auto-generated method stub
 		
-		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		myFrame.getContentPane().setLayout(new FlowLayout());
-		
-		MTextMenu menu = new MTextMenu(myFrame);
-		myFrame.setJMenuBar(menu);
-		
-		
-		JTextArea rightTextArea = new JTextArea("right", AREA_ROWS, AREA_COLUMNS);
-		rightTextArea.setLineWrap(true);
-		JScrollPane rightScrollPane = new JScrollPane(rightTextArea);
-		
-		JTextArea leftTextArea = new JTextArea("left", AREA_ROWS, AREA_COLUMNS);
-		leftTextArea.setLineWrap(true);
-		JScrollPane leftScrollPane = new JScrollPane(leftTextArea);
-		
-		JPanel leftPanel = new JPanel();
-		leftPanel.add(leftScrollPane);
-		myFrame.getContentPane().add(leftPanel); 
-		
-		JPanel rightPanel = new JPanel();
-		rightPanel.add(rightScrollPane);
-		myFrame.getContentPane().add(rightPanel);
-		
-		myFrame.setVisible(true);
 	}
 }
